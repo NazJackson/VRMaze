3ed5dea1fc9a591ac2f949502b7384d13361c3bb
Nazhir <jackson.nazhir@gmail.com>
2016-12-13 10:44:49 -0500

    finished mini maze game forgot to add to github

---
+++ Assets/Controller.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : GazeInputModule  {
+
+	private bool walking =false;
+	private Vector3 spawnPoint;
+	IGvrGazePointer pointer;
+
+	// Use this for initialization
+
+	void OnGazeDisable(){
+		
+	}
+	void Start () {
+		spawnPoint = transform.position;
+		
+	
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if(walking){
+			transform.position = transform.position + Camera.main.transform.forward * .5f * Time.deltaTime;
+	
+	}
+		if (transform.position.y < -10f) {
+			transform.position = spawnPoint;
+		}
+		Ray ray = Camera.main.ViewportPointToRay (new Vector3 (.5f, .5f, 0));
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.collider.name.Contains ("Plane")) {
+				walking = false;
+			} else {
+				walking = true;
+			}
+		}
+	}
+}
